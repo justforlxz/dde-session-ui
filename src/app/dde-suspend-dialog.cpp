dc0944c4e223a22f4a10eaa9629a639ef9a7dec4
justforlxz <justforlxz@gmail.com>
2019-04-08 10:36:50 +0800

    refactor(cmake): add dde-suppend-dialog

--- src/app/dde-suspend-dialog.cpp
+++ src/app/dde-suspend-dialog.cpp
@@ -23,7 +23,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "suspenddialog.h"
+#include "dde-suspend-dialog/suspenddialog.h"
 #include <QApplication>
 #include <QTranslator>
 
