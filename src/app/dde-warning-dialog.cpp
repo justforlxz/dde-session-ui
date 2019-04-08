dee438f505b6e6237bc3563043ef524aa1a43c9e
justforlxz <justforlxz@gmail.com>
2019-04-08 13:40:23 +0800

    refactor(cmake): add dde-warning-dialog

--- src/app/dde-warning-dialog.cpp
+++ src/app/dde-warning-dialog.cpp
@@ -23,7 +23,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "warningdialog.h"
+#include "dde-warning-dialog/warningdialog.h"
 #include <DApplication>
 #include <QTranslator>
 #include <QDBusConnection>
